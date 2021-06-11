0220929be67bcc730f727f30ad2c190f75f86230
Dustin Van Buskirk <dev@vanbuskirk.me>
2021-06-11 12:30:20 -0700

    bump sleep up some

--- lib/cf-demo-bot.py
+++ lib/cf-demo-bot.py
@@ -145,8 +145,8 @@ def main():
     merge_pull_request = None
     while merge_pull_request is None:
         try:
-            print('Waiting 30 seconds for Pull Request builds')
-            time.sleep(30)
+            print('Waiting 120 seconds for Pull Request builds')
+            time.sleep(120)
             merge_pull_request = pull_request.merge(commit_title='Freshbot Demo Automation', commit_message='Committed by Codefresh Freshbot', merge_method='merge')
         except:
             pass
